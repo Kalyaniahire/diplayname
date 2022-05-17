2e5046c76c74933edcfae2175621479964ce2fa6
Kalyaniahire <104428887+Kalyaniahire@users.noreply.github.com>
2022-05-17 11:07:15 +0530

    Create diplayname.py

---
+++ diplayname.py
@@ -0,0 +1,4 @@
+def diplaydetatils(name,created_name):
+  print("hello,My name is {0}.".format(name))
+  print("I am created thi app in {0}.".format(created_name))
+diplaydetatils('Kalyani Ahire',17-5-2022)
